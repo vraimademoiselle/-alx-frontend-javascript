ff37b42f434e9dace261b4a4d7e927fe25a19aeb
vraimademoiselle <mademoisellebone@gmail.com>
2022-12-21 22:14:51 +0300

    all files

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,5 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => [resolve, reject]);
+export default function 
+getResponseFromAPI() {
+    return new Promise((resolve, reject) => 
+    [resolve, reject]);
   }
