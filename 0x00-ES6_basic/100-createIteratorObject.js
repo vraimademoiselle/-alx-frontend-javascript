646ea110d8d3aa9a8d1a9bd92b913a186a4ee795
vraimademoiselle <mademoisellebone@gmail.com>
2022-12-21 21:29:00 +0300

    corrections

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,12 @@
 export default function createIteratorObject(report) {
-    const emps = [];
+  const emps = [];
+  // eslint-disable-next-line
+	for (const dep of Object.keys(report.allEmployees)) {
     // eslint-disable-next-line
-      for (const dep of Object.keys(report.allEmployees)) {
-      // eslint-disable-next-line
-          for (const emp of report.allEmployees[dep]) {
-        emps.push(emp);
-      }
+		for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
     }
-  
-    return emps;
   }
+
+  return emps;
+}
