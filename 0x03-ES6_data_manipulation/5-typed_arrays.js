4f61be5943c0bc2f5f3c8cecd5a29d72e68d3434
vraimademoiselle <mademoisellebone@gmail.com>
2023-01-03 13:02:28 +0300

    corrections\..

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  const arr = new ArrayBuffer(length);
-  const val = new DataView(arr);
+  const buffer = new ArrayBuffer(length);
+  const dv = new DataView(buffer);
 
-  try {
-    val.setInt8(position, value);
-  } catch (e) {
-    throw Error("Position outside range");
-  }
-  return val;
+  // check if position outside range
+  if (position >= length) throw new Error('Position outside range');
+  // set the new value in the desired position
+  dv.setInt8(position, value);
+
+  return dv;
 }
--- 0x03-ES6_data_manipulation/README.md
+++ 0x03-ES6_data_manipulation/README.md
@@ -1,4 +1,5 @@
-0x03. ES6 data manipulation
-Resources 
-Map
-How to Use Map, Filter, & Reduce in JavaScript
+# 0x03. ES6 data manipulation 
+
+# Resources
+- [Map](https://www.youtube.com/watch?v=G6J2kl1aVao)
+- [How to Use Map, Filter, & Reduce in JavaScript](https://www.youtube.com/watch?v=rLrgj6jJ5ug)
